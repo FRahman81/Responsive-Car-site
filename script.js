fae2b6002822d1d5ee67367d217613c50df7ec6e
FRahman81 <fazlurahman818791@gmail.com>
2023-02-28 19:04:18 +0530

    slow

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.querySelector('#search-icon').onclick = () =>{
     search.classList.toggle('active');
 }
 
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    menu.classList.toggle('active');
+}
+
 let header = document.querySelector('header');
 window.addEventListener('scroll' , ()=>{
     window.classList.toggle('shadow',window.scrollY > 0);
