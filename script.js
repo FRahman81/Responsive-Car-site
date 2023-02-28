d443efc922d8d396f3b284f1118e47e6d432789b
FRahman81 <fazlurahman818791@gmail.com>
2023-02-28 18:18:45 +0530

    style

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () =>{
     search.classList.toggle('active');
 }
+
+let header = document.querySelector('header');
+window.addEventListener('scroll' , ()=>{
+    window.classList.toggle('shadow',window.scrollY > 0);
+});
